030b026402e9c059ab98de9c78231513c1e66fb5
Sushma998877 <syamalasushma15@gmail.com>
2024-09-20 11:29:07 +0530

    Add files via upload

---
+++ test7.py
@@ -0,0 +1,13 @@
+def subract(a,b):
+    return(a-b)
+
+import unittest
+
+class subractclass(unittest.TestCase):
+    def test1(self):
+
+        result=subtract(10,7)
+        if(result==3):
+         result True
+
+
